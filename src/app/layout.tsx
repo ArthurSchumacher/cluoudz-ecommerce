ceb55989a00eb80b572feb0f2c7f3a4cdfb9dc81
Arthur Schumacher <arthur.s.cunha00@gmail.com>
2024-04-19 19:10:20 -0400

    feat: Better SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "E-Commerce",
+  title: "Pantanal Headshop",
   description: "Pantanal Headshop, sua tabacaria online.",
 };
 
